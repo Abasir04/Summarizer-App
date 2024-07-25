f585a867d148aaa6fb122a1cc1c787ee965c7c39
Abasir <adiadebayo1@gmail.com>
2024-07-25 22:29:59 +0100

    Hero Section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
 
+import Content from './components/Content'
+import Hero from './components/Hero'
+
 function App() {
 
   return (
-    <>
-    </>
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Content />
+      </div>
+    </main> 
   )
 }
 
