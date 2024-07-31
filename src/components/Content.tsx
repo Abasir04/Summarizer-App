7d4383932bfeb150a6a56ac0db8665880afba12a
Abasir <adiadebayo1@gmail.com>
2024-07-31 21:54:12 +0100

    Finished Summarizer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -11,7 +11,11 @@ const Content = () => {
     url: '',
     summary: ''
   });
-  const [allArticles, setAllArticles] = useState([])
+  const [allArticles, setAllArticles] = useState<{ url: string; summary: string; }[]>([])
+  const [copied, setCopied] = useState('')
+
+  const [getSummary, { error, isLoading}] = useLazyGetUrlSummaryQuery() 
+  
 
   useEffect(() => {
     const articlesFromLocalStorage: string | null = localStorage.getItem('articles')
@@ -23,8 +27,6 @@ const Content = () => {
   }, [])
   
 
-  const [getSummary ] = useLazyGetUrlSummaryQuery() 
-  {/*, { error, isLoading} */} 
 
   const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -42,6 +44,12 @@ const Content = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticle))
     }
+  }
+  
+  const handleCopy = (copyUrl: string) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(''), 5000)
   }
 
   return (
@@ -68,9 +76,56 @@ const Content = () => {
             <MdOutlineKeyboardArrowRight />
           </button>
         </form>
-      {/* Browse URL History */}
+
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div onClick={() => handleCopy(item.url)} className="copy_btn">
+                <img 
+                  src={copied === item.url ? tick : copy} 
+                  alt="copy_icon"
+                  className='w-[40%] h-[40%] object-contain' />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
+
       {/* Show Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isLoading ? (
+          <img src={loader} alt="load_icon" className='w-[40%] h-[40%] object-contain'/>
+        ) : error ? (
+          <p className="font-satoshi font-bold text-black text-center">
+            Well, that's not supposed to happen
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              (error?.data?.error)
+            </span>
+          </p>
+        ) : (article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article
+                <span className='blue_gradient'>
+                  Summary
+                </span>
+              </h2>
+              <div className="summary_box">
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
